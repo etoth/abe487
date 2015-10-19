0cf409c8c641d5096e6a3ceb3d5c2a70446ca423
Ken Youens-Clark <kyclark@gmail.com>
2015-10-19 15:36:26 -0700

    suggestions, nice job

--- week7/04-kmer-count.pl
+++ week7/04-kmer-count.pl
@@ -5,7 +5,7 @@ use autodie;
 use Data::Dumper;
 use feature 'say';
 
-my $seq       = shift or die "Please give me a sequence.\n";
+my $seq       = shift or die "Please provide a sequence.\n";
 my $kmer_size = shift || 3;
 
 if (-e $seq) {
@@ -24,23 +24,31 @@ for (my $i = 0; $i < $numkmer; $i++) {
 	my $kmer = substr($seq, $i, $kmer_size);
 	$kmers {$kmer}++
 }
-printf "%-15s %10s\n", "Sequence length", "$len";
-printf "%-15s %10s\n", "Mer size", "$kmer_size";
-printf "%-15s %10s\n", "Number of kmers", "$numkmer";
+printf "%-15s %10s\n", "Sequence length", $len;
+printf "%-15s %10s\n", "Mer size", $kmer_size;
+printf "%-15s %10s\n", "Number of kmers", $numkmer;
 
 my $size = scalar keys %kmers;
-printf "%-15s %10s\n", "Unique Kmers", "$size\n";
+printf "%-15s %10s\n", "Unique Kmers", $size;
 my $count1;
 for my $sortkey (keys %kmers) {
 	if ($kmers{$sortkey} == 1) {
     $count1++;
-    printf "%-15s %10s\n", "Num. singletons", "$count1 \n";
   }
 }
+printf "%-15s %10s\n", "Num. singletons", $count1;
 print "Most abundant\n";
 my $i = 0;
 foreach my $sortkey (sort {$kmers{$b} <=> $kmers{$a} || $a cmp $b} keys %kmers) {
+    last if $kmers{$sortkey} == 1;
 	last if ($i++) == 10;
 
 	printf "%-15s %10s\n", $sortkey, $kmers{$sortkey};
 }
+
+__END__
+
+I made a few corrections as you had way too much output.  Check the "test-out"
+file for more info on what you missed.
+
+One point off.

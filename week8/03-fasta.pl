c7ba457f267836347d537f5cb753edf11d91976a
Ken Youens-Clark <kyclark@gmail.com>
2015-10-30 09:56:51 -0700

    notes and suggestions

--- week8/03-fasta.pl
+++ week8/03-fasta.pl
@@ -3,17 +3,17 @@ use strict;
 use warnings;
 use autodie;
 use feature 'say';
-my $i = 0;
+my $i    = 0;
 my $file = shift @ARGV;
 my $pattern;
 open my $fh, '<', $file;
 while (my $line = <$fh>) {
-        chomp $line;
-        $pattern = '>';
-        if ($line =~ s/^$pattern//) {
-        		$i++;
-                print "$i: $line \n";
-        }
+    chomp $line;
+    $pattern = '>';
+    if ($line =~ s/^$pattern//) {
+        $i++;
+        print "$i: $line\n";
+    }
 
 }
 printf "Found %s sequence%s.\n", $i, $i == 1 ? '' : 's';
--- week8/04-restriction.pl
+++ week8/04-restriction.pl
@@ -1,22 +1,20 @@
-#!/usr/bin/perl                                                                                                   
-use warnings;
+#!/usr/bin/env perl                                                                                                   
 use strict;
+use warnings;
 
 my $infile = 'sequence.txt';
 my $seq;
 
-open (my $fh, '<', $infile) || die "Cannot open infile $!";
+open(my $fh, '<', $infile) || die "Cannot open infile $!";
 
-while( my $line = <$fh>)
-  {
-  chomp $line;
-  if ($line !~ m/^>/)
-    {
-    $seq .= $line;
-  }
+while (my $line = <$fh>) {
+    chomp $line;
+    if ($line !~ m/^>/) {
+        $seq .= $line;
+    }
 }
 close($fh);
-print $seq,"\n";
+print $seq, "\n";
 my @cut_sites = $seq =~ m/([AG]AATT[CT])/g;
 
 print "@cut_sites\n";
